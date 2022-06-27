7640cd863c3e9c18b32ef189961dc4ca86e4545c
Daniel <67271804+Prigunoff@users.noreply.github.com>
2022-06-27 19:10:37 +0300

    Update output to terminal  * from  db

--- src/cryptoPortfolio/Main.java
+++ src/cryptoPortfolio/Main.java
@@ -14,29 +14,23 @@ public class Main {
     private static String tokenName;
     private static Double[] outputCrypto;
     public static void main(String[] args) {
+        DBConnection crypto = new DBConnection();
         Scanner scan = new Scanner(System.in);
         System.out.println("Set token name: ");
         String value= scan.nextLine();
         getMainData(value);
-
         //Вычисления
         CryptoMath math = new CryptoMath(tokenName,depAmount,tokenPriceAtBuyMoment,priceResultXlm);
         math.outputResultCalc();
+        System.out.println();
         // Цифры для ввода как тест 17.35  и 0.39(для отрицательного) 0.001 ( для положительного)
-
         //Готовый массив даблов
         outputCrypto = math.returnResultDouble();
-        ////////////////////////////Sign to db////////////////////////////////////////////
-
-        DBConnection crypto = new DBConnection();
-
+        //Добавление в бд
         crypto.dbInsertion(tokenName,outputCrypto[0],outputCrypto[1],outputCrypto[2]
                 ,outputCrypto[3],outputCrypto[4],outputCrypto[5]);
-
-        //Db Result viev
+        //Вывод бд в терминал
         crypto.getDbResult();
-
-
     }
     private static void getMainData(String value){
         CryptoData data = new CryptoData();
